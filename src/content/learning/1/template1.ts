3b4029fb578cbfd983f82a841f13c7dba657e51a
Damian Kalka <damian.kalka@netguru.com>
2021-12-11 00:17:50 +0100

    add initial reading learning tuts

---
+++ src/content/learning/1/template1.ts
@@ -0,0 +1,16 @@
+export const challengeTitle = 'asdf';
+
+export const codeTemplate = () => `
+import * as p5 from 'p5';
+
+function setup() {
+  createCanvas(100, 100);
+}
+
+function draw() {
+  background(0, 255, 0);
+}
+
+window.setup = setup
+window.draw = draw
+`;
