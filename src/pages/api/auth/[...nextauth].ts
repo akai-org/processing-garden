60173f28f76e869cb4d64669293612dfd0c77661
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-12-11 01:27:47 +0100

    Trying to work with oauth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || '',
+      clientSecret: process.env.GOOGLE_SECRET || '',
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      console.log({ user });
+
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
