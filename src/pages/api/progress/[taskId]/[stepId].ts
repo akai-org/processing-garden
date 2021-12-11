d575f0e05107195d7293aef8d53fedf9057085d1
roertbb <robert.banaszak1997@gmail.com>
2021-12-11 01:28:00 +0100

    api routes

---
+++ src/pages/api/progress/[taskId]/[stepId].ts
@@ -0,0 +1,40 @@
+import { z } from 'zod';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import db from 'db';
+
+export default async function progress(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const session = await getSession({ req });
+  const queryParams = z
+    .object({
+      query: z.object({
+        taskId: z.string(),
+        stepId: z.number(),
+      }),
+    })
+    .safeParse(req);
+
+  if (req.method === 'POST' && queryParams.success) {
+    const { stepId, taskId } = queryParams.data.query;
+
+    try {
+      await db.progress.create({
+        data: {
+          taskId,
+          stepId,
+          // @ts-ignore
+          userId: session.id,
+        },
+      });
+
+      res.status(200).json({ success: true });
+    } catch (error) {
+      res.status(500).json({ error, message: 'Failed to set progress' });
+    }
+  }
+
+  return res.status(400).json({ error: 'Invalid input' });
+}
---
+++ src/pages/api/sandbox/[id]/joined.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import db from 'db';
+
+export default async function progress(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const session = await getSession({ req });
+  const queryParams = z
+    .object({ query: z.object({ id: z.string() }) })
+    .safeParse(req);
+
+  if (req.method === 'POST' && queryParams.success) {
+    const { id } = queryParams.data.query;
+
+    try {
+      await db.progress.create({
+        data: {
+          taskId,
+          stepId,
+          // @ts-ignore
+          userId: session.userId,
+        },
+      });
+
+      res.status(200).json({ success: true });
+    } catch (error) {
+      res.status(500).json({ error, message: 'Failed to set progress' });
+    }
+  }
+
+  return res.status(400).json({ error: 'Invalid input' });
+}
