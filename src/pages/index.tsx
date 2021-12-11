387801778144598aaaf4cae62764f438bd8428d7
Damian Kalka <damian.kalka@netguru.com>
2021-12-11 23:38:55 +0100

    hero changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,17 @@ import {
   Button,
   chakra,
   Icon,
-  Image,
   Stack,
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import NextImage from 'next/image';
 
 export default function Home() {
+  const session = useSession();
+  const isAuthenticated = session.status === 'authenticated';
+
   return (
     <Box px={8} py={24} mx="auto" width="fit-content">
       <Box
@@ -51,7 +53,8 @@ export default function Home() {
           fontSize={{ base: 'lg', md: 'xl' }}
           color={useColorModeValue('gray.600', 'gray.300')}
         >
-          Ucz, graj, baw się razem z innymi, zdobywaj punkty i osiągnięcia oraz
+          Ucz, graj, baw się, zdobywaj punkty i osiągnięcia oraz rywalizuj z
+          innymi programując!
         </chakra.p>
         <Stack
           direction={{ base: 'column', sm: 'row' }}
@@ -59,43 +62,31 @@ export default function Home() {
           spacing={2}
           justifyContent={{ sm: 'left', md: 'center' }}
         >
-          <Button
-            as="a"
-            variant="solid"
-            colorScheme="brand"
-            display="inline-flex"
-            alignItems="center"
-            justifyContent="center"
-            w={{ base: 'full', sm: 'auto' }}
-            mb={{ base: 2, sm: 0 }}
-            size="lg"
-            cursor="pointer"
-            onClick={() => signIn()}
-          >
-            Rozpocznij
-            <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor">
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </Icon>
-          </Button>
+          {!isAuthenticated && (
+            <Button
+              as="a"
+              variant="solid"
+              colorScheme="brand"
+              display="inline-flex"
+              alignItems="center"
+              justifyContent="center"
+              w={{ base: 'full', sm: 'auto' }}
+              mb={{ base: 2, sm: 0 }}
+              size="lg"
+              cursor="pointer"
+              onClick={() => signIn()}
+            >
+              Rozpocznij
+              <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor">
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </Icon>
+            </Button>
+          )}
         </Stack>
-      </Box>
-      <Box
-        w={{ base: 'full', md: 10 / 12 }}
-        mx="auto"
-        mt={20}
-        textAlign="center"
-      >
-        <Image
-          w="full"
-          rounded="lg"
-          shadow="2xl"
-          src="https://kutty.netlify.app/hero.jpg"
-          alt="Hellonext feedback boards software screenshot"
-        />
       </Box>
     </Box>
   );
